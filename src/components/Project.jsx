aa3b0b9d0af555d7303771a3c3af3adfaa441dc7
Gwenn RT <gwenncreative@2020-MBP.attlocal.net>
2024-04-21 20:22:47 -0500

    added to Project.jsx and created addtional jsx files

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
 
 function Project() {
+  return (
+    <div className='projectSection'>
+      <div className="sectionTitle">
+        <h2>Projects</h2>
+      </div>
 
-    return(
-        <div>
-        
+      <div>
+        <div className="card">
+          <a
+            className="cardTitle"
+            href="https://github.com/gwennr-t/Random-Password-Generator"
+            style="text-decoration: none"
+          >
+            Random Password Generator
+          </a>
         </div>
-    );
+
+        <div className="card">
+          <a className="cardTitle" href="https://github.com/gwennr-t/Work-Day-Scheduler" style="text-decoration: none">
+            Work Day Scheduler
+          </a>
+        </div>
+
+        <div className="card">
+          <a className="cardTitle" href="https://github.com/gwennr-t/code-quiz" style="text-decoration: none">
+            Code Quiz
+          </a>
+        </div>
+
+        <div className="card">
+          <a className="cardTitle" href="https://github.com/gwennr-t/Weather-Dashboard" style="text-decoration: none">
+            Weather Dashboard
+          </a>
+        </div>
+
+        <div className="card">
+          <a className="cardTitle" href="https://github.com/gwennr-t/professional-readme-generator" style="text-decoration: none">
+            Professional README.md Generator
+          </a>
+        </div>
+
+        <div className="card">
+          <a className="cardTitle" href="https://github.com/gwennr-t/text-editor" style="text-decoration: none">
+            Text Editor
+          </a>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default Project;
