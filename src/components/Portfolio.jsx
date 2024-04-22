9175c985d05f7eabe69511ada83b88851fe0c8b4
Gwenn RT <gwenncreative@2020-MBP.attlocal.net>
2024-04-22 16:11:58 -0500

    change made to Portfolio.jsx

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Project from './Project';
+
+function Portfolio() {
+    return(
+        <div>
+            <div className='portfolioTitle'>
+                <h2>Portfolio</h2>
+            </div>
+            <div className='portfolioItem'>
+                <div className='itemImage'>
+                    <a href=''></a>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Portfolio;
