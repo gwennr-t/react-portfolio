1f496e5f6449869be0bf20b668a181c60a503fa8
Gwenn RT <gwenncreative@2020-MBP.attlocal.net>
2024-04-21 19:33:26 -0500

    Added to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
 
 function Footer() {
-
-    return (
-        <footer>
-
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div id="footerContent">
+        <p>Portfolio Made by Gwenn R.</p>
+        <p>
+          <a href="#" style="text-decoration: none">
+            GitHub
+          </a>
+          <a href="#" style="text-decoration: none">
+            LinkedIn
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
