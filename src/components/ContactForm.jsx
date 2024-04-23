e97cd756dca32946729e8ca449b9d826fd11c0ac
Gwenn RT <gwenncreative@2020-MBP.attlocal.net>
2024-04-22 23:23:39 -0500

    Edits made to ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,56 +1,54 @@
-import React from 'react';
-import {useState} from 'react';
+import React from "react";
+import { useState } from "react";
 
-function ContactForm(props) {
-    const [input, setInput] = useState('');
+function ContactForm() {
+  const [input, setInput] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        props.onSubmit ({
-            id: Math.random(Math.floor() * 1000),
-            text: input,
-        });
+    props.onSubmit({
+      id: Math.random(Math.floor() * 1000),
+      text: input,
+    });
 
-        setInput('');
-    };
+    setInput("");
+  };
 
-    const handleChange = (e) => {
-        setInput(e.target.value);
-    };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-    return !props.edit ? (
-        <div>
-            <form className='contactForm' onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Enter your name"
-                    value={input}
-                    name="Name"
-                    className="name"
-                    onChange={handleChange}
-                ></input>
-                <input
-                    type="text"
-                    placeholder="Enter your email"
-                    value={input}
-                    name="Email"
-                    className="email"
-                    onChange={handleChange}
-                ></input>
-                <input
-                    type="text"
-                    placeholder="Type your message here"
-                    value={input}
-                    name="Messagee"
-                    className="message"
-                    onChange={handleChange}
-                ></input>
-            </form>
-        </div>
-    ) : (
-        
-    );
+  return (
+    <div>
+      <form className="contactForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={input}
+          name="Name"
+          className="name"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="text"
+          placeholder="Enter your email"
+          value={input}
+          name="Email"
+          className="email"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="text"
+          placeholder="Type your message here"
+          value={input}
+          name="Messagee"
+          className="message"
+          onChange={handleChange}
+        ></input>
+      </form>
+    </div>
+  );
 }
 
 export default ContactForm;
